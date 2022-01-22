bffd3bd211ad757067f5fb2728197aecc5c602de
Ting Ting Chen <tin2294@gmail.com>
2022-01-22 19:36:32 +0100

    nutritional info"

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,7 +3,8 @@ require 'fatsecret'
 class MealsController < ApplicationController
   def summary
     FatSecret.init('9f99ea26f4f44f1ab8ec88b6e5a1bb68', '2f5e533c27fe48e28342043db649df05')
-    @info = FatSecret.search_food('milk')
+    @food = FatSecret.search_food('milk')
+    @info = FatSecret.food(800)
   end
 
   def home
