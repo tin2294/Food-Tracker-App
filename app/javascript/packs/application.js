01990892d745bf383bd5115acafa236f260d8f66
Ting Ting Chen <tin2294@gmail.com>
2022-01-23 20:16:33 +0100

    tailwind installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "../application.css";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import "../application.css";
+import "stylesheets/application";
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+require("stylesheets/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+purge: [
+  "./app/**/*.html.erb",
+  "./app/helpers/**/*.rb",
+  "./app/javascript/**/*.js",
+  "./app/javascript/**/*.vue",
+];
+
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
